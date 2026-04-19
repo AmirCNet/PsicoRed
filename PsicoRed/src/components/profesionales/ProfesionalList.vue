0307b3967e743caf5bd4c338241c44f50ae1fb86
Eve <evelyninostroza094@gmail.com>
2026-04-19 17:24:15 -0300

    Nueva rama 'profesionales' con pantalla y primeros cambios

---
+++ PsicoRed/src/components/profesionales/ProfesionalList.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <h2>Listado de Profesionales</h2>
+    <p>Acá van a aparecer los profesionales...</p>
+  </div>
+</template>
---
+++ PsicoRed/src/views/Profesionales.vue
@@ -0,0 +1,10 @@
+<script setup>
+import ProfesionalList from '../components/profesionales/ProfesionalList.vue'
+</script>
+
+<template>
+  <div>
+    <h1>Gestión de Profesionales</h1>
+    <ProfesionalList />
+  </div>
+</template>
